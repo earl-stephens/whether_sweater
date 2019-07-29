355cc29f7d3372533a174c9fafa932ed25b8cf51
Earl Stephens <stephensearl@hotmail.com>
2019-07-29 15:57:24 -0600

    Update user_spec.rb
    deleted some extra comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,18 +4,4 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of :email }
   end
-
-  # describe 'instance methods' do
-  #   it '.create_api_key'do
-  #     user = User.new(email: 'john@doe.com',
-  #                     password: 'password')
-  #
-  #     expect(user.api_key).to eq(nil)
-  #
-  #     user.save
-  #     user.reload
-  #     binding.pry
-  #     expect(user.api_key).to_not eq(nil)
-  #   end
-  # end
 end
