04fce9b6264a806476f69161860727f1e004afdc
earl-stephens <stephensearl@hotmail.com>
2019-07-30 21:46:35 -0600

    refactored to create a background facade

---
+++ app/facades/background_facade.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# facade for backgrounds
+class BackgroundFacade
+
+  def initialize(location)
+    @location = location
+  end
+
+  def grab_image_url
+    service_data[:hits][0][:largeImageURL]
+  end
+
+  private
+
+  def service
+    @_service ||= PixabayService.new
+  end
+
+  def service_data
+    @_service_data ||= service.city_image(@location)
+  end
+
+end
